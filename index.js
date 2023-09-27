e3cb26eab2e07bfc91c385b8d960d4c71d191f50
Andrew <andrewstaveley14@gmail.com>
2023-09-27 15:16:01 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+] 
+
+function superbowlWin(array) {
+    let object
+    function winFinder(element) {
+        if (element.result === "W") {
+            return true
+        } else {
+            return undefined
+        }
+    }
+    if (array.find(winFinder) === undefined) {
+        object = undefined
+        return object
+    } else {
+        object = array.find(winFinder)
+        return object.year
+    }
+}
